2d5f47d48334cb2aae419fac71488aa511824b3b
Jess Neary <jessicafneary@gmail.com>
2019-08-05 20:52:11 +0200

    Connected props and actions to products component

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,22 +1,38 @@
 import React, { Component } from "react";
-import { mapStateToProps, mapDispatchToProps } from "redux";
+import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 
+import { actionTypes } from "../../actions";
+
 class Products extends Component {
-    state = {
-        productList: []
-    };
+    componentDidMount() {
+        this.props.getProducts();
+    }
     render() {
-        const productList = this.state.productList;
+        const productList = this.props.products;
         return (
             <div>
                 <div>Products</div>
                 {productList[0] && productList.map(i => (
-                    <p>{i.id}</p>
+                    <p key={i.id}>{i.id}</p>
                 ))
                 }
             </div>
         )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        products: state.products,
+        details: state.details,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getProducts: bindActionCreators(actionTypes.getAll, dispatch),
+        getDetails: (id) => bindActionCreators(actionTypes.getDetails, dispatch)
     }
 }
 
